8dda04fd318df7d583cb3b159f331b5106db8c9c
Sander van Rijn <svr003@gmail.com>
2022-09-16 22:03:27 +0100

    Add simulate random game function

--- src/battleship/game.py
+++ src/battleship/game.py
@@ -1,21 +1,17 @@
 """Play a game of Battleship"""
 
-from itertools import count
+from itertools import count, product
+from random import shuffle
+from typing import Sequence
 
 from battleship.board import Board
 from battleship.ship import ShipPartState
 
 
-def play_1p_game():
+def play_1p_game(fleet: Sequence[int]):
     """Play a '1p' game: sink a randomly placed fleet in the fewest moves"""
     b = Board()
-    print("what fleet should be placed? (leave empty for default)")
-    fleet_str = input()
-    if fleet_str:
-        fleet = list(map(int, fleet_str.strip('\n').split(',')))
-        b.place_random_fleet(fleet)
-    else:
-        b.place_random_fleet()
+    b.place_random_fleet(fleet)
 
     for move in count(1):
         print(b.display(public=True))
@@ -34,6 +30,27 @@ def play_1p_game():
             print(b.display(public=True))
             break
 
+def simulate_random_strategy(fleet: Sequence[int]) -> int:
+    """Simulate a randomly played game: shoot all squared once in random order"""
+    b = Board()
+    b.place_random_fleet(fleet)
+
+    shots = list(product(range(b.size), range(b.size)))
+    shuffle(shots)
+    for move, (r,c) in enumerate(shots):
+        b.shoot(r,c)
+        if b.fleet_sunk():
+            return move
+
 
 if __name__ == "__main__":
-    play_1p_game()
+
+    print("what fleet should be used? (leave empty for default = [5, 4,4, 3,3,3, 2,2,2,2])")
+    fleet_str = input()
+    if fleet_str:
+        fleet = list(map(int, fleet_str.strip('\n').split(',')))
+    else:
+        fleet = None
+
+    # play_1p_game(fleet)
+    print(f"random game for fleet took {simulate_random_strategy(fleet)} moves")
